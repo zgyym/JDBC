8222d1fd87da89a15347a986688e098d44f1f6dd
zgyym <2064635072@qq.comm>
2022-02-27 19:30:46 +0800

    '事务'

---
+++ Test/src/transaction/TransactionTest.java
@@ -0,0 +1,176 @@
+package transaction;
+
+import PreperadStatementCRUD.Util.JDBCUtils;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+
+public class TransactionTest {
+
+    //******************未考虑数据库事务情况下的转账操作**************************
+    /*
+     * 针对于数据表user_table来说：
+     * AA用户给BB用户转账100
+     *
+     * update user_table set balance = balance - 100 where user = 'AA';
+     * update user_table set balance = balance + 100 where user = 'BB';
+     */
+    @Test
+    public void testUpdate(){
+
+        String sql1 = "update user_table set balance = balance - 100 where user = ?";
+        update(sql1, "AA");
+
+        //模拟网络异常
+        System.out.println(10 / 0);
+
+        String sql2 = "update user_table set balance = balance + 100 where user = ?";
+        update(sql2, "BB");
+
+        System.out.println("转账成功");
+    }
+
+    // 通用的增删改操作---version 1.0
+    public int update(String sql, Object... args) {// sql中占位符的个数与可变形参的长度相同！
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            // 1.获取数据库的连接
+            conn = JDBCUtils.getConnection();
+            // 2.预编译sql语句，返回PreparedStatement的实例
+            ps = conn.prepareStatement(sql);
+            // 3.填充占位符
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);// 小心参数声明错误！！
+            }
+            // 4.执行
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            // 5.资源的关闭
+            JDBCUtils.closeResources(conn, ps);
+
+        }
+        return 0;
+
+    }
+
+    //********************考虑数据库事务后的转账操作*********************
+
+    @Test
+    public void testUpdateWithTx() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            System.out.println(conn.getAutoCommit());//true
+            //1.取消数据的自动提交
+            conn.setAutoCommit(false);
+
+            String sql1 = "update user_table set balance = balance - 100 where user = ?";
+            update(conn,sql1, "AA");
+
+            //模拟网络异常
+            System.out.println(10 / 0);
+
+            String sql2 = "update user_table set balance = balance + 100 where user = ?";
+            update(conn,sql2, "BB");
+
+            System.out.println("转账成功");
+
+            //2.提交数据
+            conn.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            //3.回滚数据
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }finally{
+            //修改其为自动提交数据
+            //主要针对于使用数据库连接池的使用
+            try {
+                conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            JDBCUtils.closeResources(conn, null);
+        }
+
+    }
+
+    // 通用的增删改操作---version 2.0 （考虑上事务）
+    public int update(Connection conn,String sql, Object... args) {// sql中占位符的个数与可变形参的长度相同！
+        PreparedStatement ps = null;
+        try {
+            // 1.预编译sql语句，返回PreparedStatement的实例
+            ps = conn.prepareStatement(sql);
+            // 2.填充占位符
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);// 小心参数声明错误！！
+            }
+            // 3.执行
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 4.资源的关闭
+            JDBCUtils.closeResources(null, ps);
+
+        }
+        return 0;
+
+    }
+
+    //通用的查询操作，用于返回数据表中的一条记录（version 2.0：考虑上事务）
+    public <T> T getInstance(Connection conn,Class<T> clazz,String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+
+            if (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给t对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResources(null, ps, rs);
+        }
+
+        return null;
+    }
+
+}
+
